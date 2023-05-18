73ada97d90fab25dfb193ca8b00cccadab717e84
RaukadSingh123 <115916325+RaukadSingh@users.noreply.github.com>
2023-05-18 08:06:44 +0530

    Add files via upload

---
+++ java/com/book/LiabraryManagementSystemApplication.java
@@ -0,0 +1,16 @@
+package com.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiabraryManagementSystemApplication {
+
+	
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(LiabraryManagementSystemApplication.class, args);
+	}
+
+}
